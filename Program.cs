61179f8a7e917a1b3708ed421faa86ea0c3a105e
jhonal28 <jalzate0128@gmail.com>
2020-08-19 19:40:11 -0500

    Explicación de polimorfismo

--- Program.cs
+++ Program.cs
@@ -13,8 +13,46 @@ namespace CoreEscuela
             var engine = new EscuelaEngine();
             engine.Inicializar();
             Printer.WriteTitle("BIENVENIDOS A LA ESCUELA");
-            Printer.Beep(10000, 500, 10);
-            imprimirCursosEscuela(engine.Escuela);    
+            //Printer.Beep(10000, 500, 10);
+            imprimirCursosEscuela(engine.Escuela);   
+
+            Printer.DrawLine(20);
+            Printer.DrawLine(20);
+            Printer.DrawLine(20);
+            Printer.WriteTitle("Pruebas de polimorfismo"); 
+
+            var alumnoTest = new Alumno{Nombre = "Claire Underwood"};
+            
+            Printer.WriteTitle("Alumno");
+            WriteLine($"Alumno: {alumnoTest.Nombre}");
+            WriteLine($"Alumno: {alumnoTest.UniqueId}");
+            WriteLine($"Alumno: {alumnoTest.GetType()}");
+
+            ObjetoEscuelaBase ob = alumnoTest;
+            Printer.WriteTitle("ObjetoEscuela");
+            WriteLine($"Alumno: {ob.Nombre}");
+            WriteLine($"Alumno: {ob.UniqueId}");
+            WriteLine($"Alumno: {ob.GetType()}");
+
+            var objDummy = new ObjetoEscuelaBase(){Nombre = "Frank Underwood"};
+            Printer.WriteTitle("ObjetoEscuela");
+            WriteLine($"Alumno: {objDummy.Nombre}");
+            WriteLine($"Alumno: {objDummy.UniqueId}");
+            WriteLine($"Alumno: {objDummy.GetType()}");
+
+            var evaluacion = new Evaluacion(){Nombre = "Evaluación de math", Nota = 4.5f};
+            Printer.WriteTitle("Evaluación");
+            WriteLine($"Evaluación: {evaluacion.Nombre}");
+            WriteLine($"Evaluación: {evaluacion.Nota}");
+            WriteLine($"Evaluación: {evaluacion.UniqueId}");
+            WriteLine($"Evaluación: {evaluacion.GetType()}");
+
+            ob = evaluacion;
+            Printer.WriteTitle("ObjetoEscuela");
+            WriteLine($"Alumno: {ob.Nombre}");
+            WriteLine($"Alumno: {ob.UniqueId}");
+            WriteLine($"Alumno: {ob.GetType()}");
+
         }
         
         private static void imprimirCursosEscuela(Escuela escuela)
